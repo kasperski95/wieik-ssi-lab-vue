14f21d54d85427a3f348ee124438741c6e03ca57
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2020-11-24 20:26:30 +0100

    prepare codebase for the test

--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Vue from 'vue';
-import mojKomponent from './App';
+import App from './App';
 
 Vue.filter('globalnyFilter', (val) => `=>${val}<=`);
 
 new Vue({
   el: '#app',
-  components: { wlasny: mojKomponent },
-  template: `
-  <div class="text-center">
-    <h1 class="bg-primary text-white p-3">
-      To jest główny plik main.js
-    </h1>
-    <wlasny />
-  </div>
-  `,
+  components: { App },
+  template: `<App />`,
 });
